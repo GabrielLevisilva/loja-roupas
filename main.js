82b47bcc86413bd5561ac9ddbc893e5a9bf1410c
Gabriel Levi <137122494+GabrielLevisilva@users.noreply.github.com>
2023-07-02 08:54:48 -0300

    vSETAS

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-const botao = document.querySelector('#seta2');
+const botao = document.querySelector('.seta2');
 const caixaevento = document.querySelector('.caixa');
 
 botao.addEventListener('click', () => {
   caixaevento.scrollLeft += 350;
 });
+
+const botao2 = document.querySelector('.seta1');
+
+botao2.addEventListener('click', () => {
+  caixaevento.scrollLeft -= 350;
+});
+
 
